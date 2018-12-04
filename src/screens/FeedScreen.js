95a39823321c424f478f7427b6fa2d124dd9aff8
Amir Ahangari <triplea1373@gmail.com>
2018-12-04 18:25:00 +0330

    Using fetch to receive data

--- src/screens/FeedScreen.js
+++ src/screens/FeedScreen.js
@@ -15,62 +15,65 @@ export default class FeedScreen extends Component {
     headerTitle: "Home"
   };
   state = {
-    data: [
-      { username: "1", liked: false },
-      { username: "2", liked: false },
-      { username: "3", liked: false },
-      { username: "4", liked: false },
-      { username: "5", liked: false }
-    ]
+    data: []
   };
+  componentDidMount() {
+    // fetch()
+    fetch("http://localhost:3000/photos")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ data });
+      })
+      .catch(({ message }) => console.log(message));
+  }
   render() {
     const imageWidth = Dimensions.get("window").width;
-    const images = this.state.data.map(({ username, liked }, index) => {
-      return (
-        <View key={index + ""}>
-          <TouchableOpacity
-            onPress={() =>
-              this.props.navigation.navigate("post", {
-                username,
-                liked,
-                uri: "https://placekitten.com/g/200/300"
-              })
-            }
-            style={styles.titleContainer}
-          >
-            <Image
-              style={styles.userAvatar}
-              source={{ uri: "https://placekitten.com/g/200/300" }}
-            />
-            <Text style={styles.title}>{username}</Text>
-            <TouchableOpacity>
-              <Ionicons name="ios-more" size={24} />
-            </TouchableOpacity>
-          </TouchableOpacity>
-          <Image
-            style={{ width: imageWidth, height: imageWidth }}
-            source={{ uri: "https://placekitten.com/g/200/300" }}
-          />
-          <View style={styles.likeContainer}>
+    const images = this.state.data.map(
+      ({ username, user_avatar, image, caption, liked }, index) => {
+        return (
+          <View key={index + ""}>
             <TouchableOpacity
-              onPress={() => {
-                // const data = this.state.data;
-                const { data } = this.state;
-                data[index].liked = !liked;
-                // this.setState({ data: data });
-                this.setState({ data });
-              }}
+              onPress={() =>
+                this.props.navigation.navigate("post", {
+                  username,
+                  liked,
+                  uri: image
+                })
+              }
+              style={styles.titleContainer}
             >
-              <Ionicons
-                name={liked ? "ios-heart" : "ios-heart-empty"}
-                size={24}
-                color={liked ? "red" : "black"}
-              />
+              <Image style={styles.userAvatar} source={{ uri: user_avatar }} />
+              <Text style={styles.title}>{username}</Text>
+              <TouchableOpacity>
+                <Ionicons name="ios-more" size={24} />
+              </TouchableOpacity>
             </TouchableOpacity>
+            <Image
+              style={{ width: imageWidth, height: imageWidth }}
+              source={{ uri: image }}
+            />
+            <View style={styles.likeContainer}>
+              <TouchableOpacity
+                onPress={() => {
+                  // const data = this.state.data;
+                  const { data } = this.state;
+                  data[index].liked = !liked;
+                  // this.setState({ data: data });
+                  this.setState({ data });
+                }}
+              >
+                <Ionicons
+                  name={liked ? "ios-heart" : "ios-heart-empty"}
+                  size={24}
+                  color={liked ? "red" : "black"}
+                />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.caption}>{caption}</Text>
           </View>
-        </View>
-      );
-    });
+        );
+      }
+    );
     return <ScrollView style={styles.container}>{images}</ScrollView>;
   }
 }
@@ -82,6 +85,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     padding: 20
   },
+  caption: { margin: 20 },
   userAvatar: { width: 64, height: 64, borderRadius: 32 },
   title: { marginLeft: 10, flex: 1 },
   likeContainer: { margin: 5, flexDirection: "row" }
