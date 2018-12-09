6c480516d7ea544daf22fd242483efab4b1ce06d
Amir Ahangari <triplea1373@gmail.com>
2018-12-09 19:03:28 +0330

    use Promise in logics

--- src/logics/index.js
+++ src/logics/index.js
@@ -1,11 +1,38 @@
 import axios from "axios";
 
+const cache = {};
+// export const fetchPhotos = async () => {
+//   if (a.photos) {
+//     console.log("read from cache");
+//     return a.photos;
+//   }
+//   return axios.get("http://localhost:3000/photos").then(({ data }) => data);
+// };
 export const fetchPhotos = () => {
-  return axios.get("http://localhost:3000/photos").then(({ data }) => data);
+  return new Promise((resolve, reject) => {
+    if (cache.photos) {
+      // const diff  = new Date() - cache.timestamp;
+      // if(diff < 50000){
+      resolve(cache.photos);
+      // }
+    }
+    axios
+      .get("http://localhost:3000/photos")
+      .then(({ data }) => {
+        cache.photos = data;
+        // cache.timestamp = new Date();
+        return data;
+      })
+      .then(data => resolve(data))
+      .catch(e => reject(e));
+  });
 };
 
 export const fetchPost = id => {
-  return axios
-    .get(`http://localhost:3000/photos?id=${id}`)
-    .then(({ data }) => data[0]);
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`http://localhost:3000/photos?id=${id}`)
+      .then(({ data }) => resolve(data[0]))
+      .catch(e => reject(e));
+  });
 };
