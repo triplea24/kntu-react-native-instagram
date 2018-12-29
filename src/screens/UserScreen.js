bec746132abb64ffb5d02baaf9446d0a76aaab20
Amir Ahangari <triplea1373@gmail.com>
2018-12-29 18:20:48 +0330

    Fix UserScreen bug

--- src/screens/UserScreen.js
+++ src/screens/UserScreen.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { View, StyleSheet, FlatList, Dimensions, Image } from "react-native";
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  Dimensions,
+  Image,
+  ActivityIndicator,
+  AsyncStorage,
+  Button
+} from "react-native";
+import { connect } from "react-redux";
 
 import ProfileImage from "../components/ProfileImage";
 import { fetchPhotos } from "../logics";
 
-export default class UserScreen extends React.Component {
-  state = {
-    posts: []
-  };
-  componentDidMount() {
-    const { username, user_avatar } = this.props.navigation.state.params;
-    fetchPhotos(username).then(posts => this.setState({ posts }));
-    this.setState({ user_avatar });
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: props.photos ? props.photos : null
+    };
   }
   renderItem = ({ item, index }) => {
     const { id, image, caption, liked } = item;
@@ -23,12 +31,21 @@ export default class UserScreen extends React.Component {
       />
     );
   };
+  componentDidMount() {
+    if (this.state.posts) return;
+    const { username } = this.props;
+    fetchPhotos(username)
+      .then(posts => this.setState({ posts }))
+      .catch(error => console.log(error.message));
+  }
   render() {
     return (
       <View style={styles.container}>
         <ProfileImage
           style={styles.userAvatar}
-          source={{ uri: this.state.user_avatar }}
+          source={{
+            uri: this.props.user_avatar
+          }}
         />
         <FlatList
           data={this.state.posts}
@@ -40,6 +57,70 @@ export default class UserScreen extends React.Component {
     );
   }
 }
+
+// const MyProfile = connect(mapStateToProps)(Profile);
+
+export class UserScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOwnProfile: !props.navigation.getParam("username") ? true : null
+    };
+  }
+  componentDidMount() {
+    if (this.state.isOwnProfile !== null) {
+      return;
+    }
+
+    // const
+    let username, user_avatar, isOwnProfile;
+    if (this.state.isOwnProfile) {
+      username = this.props.username;
+      user_avatar = this.props.userAvatar;
+      isOwnProfile = true;
+    } else {
+      username = this.props.navigation.getParam("username");
+      user_avatar = this.props.navigation.getParam("user_avatar");
+      isOwnProfile = false;
+    }
+    fetchPhotos(username)
+      .then(posts =>
+        this.setState({ isOwnProfile, posts, loaded: true, user_avatar })
+      )
+      .catch(error => console.log(error.message));
+  }
+
+  render() {
+    if (this.state.isOwnProfile === null) {
+      return (
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <ActivityIndicator />
+        </View>
+      );
+    }
+    if (this.state.isOwnProfile) {
+      return <Profile {...this.props} posts={this.state.posts} />;
+    }
+    const username = this.props.navigation.getParam("username");
+    const user_avatar = this.props.navigation.getParam("user_avatar");
+    return (
+      <Profile
+        username={username}
+        user_avatar={user_avatar}
+        posts={this.state.posts}
+      />
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  username: state.user.username,
+  user_avatar: state.user.user_avatar
+});
+
+export default connect(mapStateToProps)(UserScreen);
 
 const styles = StyleSheet.create({
   container: {
